699297c9da81b678811b742332b29d7421a036d2
abir <abirchandrabhushan@gmail.com>
2019-06-02 16:29:04 +0530

    Refined search algorithm

--- eventPage.js
+++ eventPage.js
@@ -1,27 +1,26 @@
-// A Wikipedia CRX with a one-click search
-
-var menuItem = {
-    "id": "Wiki",
-    "title": "Ask Wikipedia",
-    "contexts": ["selection"]
-};
-
-chrome.contextMenus.create(menuItem);
+var title = "Ask Wikipedia";
+var contexts = ["selection"];
+chrome.contextMenus.create({
+  "id": "wiki",
+  "title": title,
+  "contexts": contexts
+});
 
 function fixedEncodeURI (str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
-    if (clickData.menuItemId == "Wiki" && clickData.selectionText){
-        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
-        var createData = {
-            "url": wikiUrl,
-            "type": "popup",
-            "top": 5,
-            "left": 5,
-            "width": screen.availWidth / 2,
+    if (clickData.menuItemId == "wiki" && clickData.selectionText){
+        var searchKey = disambiguate(clickData.selectionText);
+        var wikiUrl = "https://en.wikipedia.org/wiki/Special:Search/" + fixedEncodeURI(searchKey);
+        var createProperties = {
+          "url": wikiUrl
         };
-        chrome.windows.create(createData, function(){});
+        chrome.tabs.create(createProperties, function(){});
     }
 });
+
+function disambiguate (str) {
+  return str;
+}
