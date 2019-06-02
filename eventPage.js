6fd0139f3f979b7684508d9eb11ae6bf87797889
abir <abirchandrabhushan@gmail.com>
2019-06-02 16:00:28 +0530

    Working crx

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,27 @@
-/*
+// A Wikipedia CRX with a one-click search
+
 var menuItem = {
-  "id": "Wiki",
-  "title": "Ask Wikipedia",
-  "contexts": ["selection"]
+    "id": "Wiki",
+    "title": "Ask Wikipedia",
+    "contexts": ["selection"]
 };
-*/
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Wiki" && clickData.selectionText){
+        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth / 2,
+        };
+        chrome.windows.create(createData, function(){});
+    }
+});
